5224587be145768174d052e2a25519623aa57e10
yFonteSeca <lf.caetanofonseca@outlook.com>
2017-08-06 09:56:45 -0300

    teste

--- index.js
+++ index.js
@@ -12,8 +12,6 @@
 
   let PREFIX = '+';
 
-  let cor-destaque = '5351170';
-
   Bot.on('message', msg => {
 
     const message = msg.content.trim();
@@ -25,27 +23,60 @@
       const suffix = message.substring(PREFIX.length + command.length).trim();
 
       // Comando
-  	  switch (command) {
-     		case 'permission':
-    			return test(msg, suffix);
-    		case 'ohayo':
-    			return ohayo(msg, suffix);
-    		default:
-    			msg.channel.send(basicembed('cor-destaque', 'Comando errado ' + msg.author + ' b-baka...'));
-    		}
+     switch (command) {
+    case 'permission':
+      return test(msg, suffix);
+    case 'ohayo':
+      return ohayo(msg, suffix);
+    case 'kiss':
+      return kiss(msg, suffix);
+    case 'say':
+      return say(msg, suffix);
+    case 'onii':
+      return onii(msg, suffix);
+    case 'ship':
+      return ship(msg, suffix);
+    case 'casar':
+      return casar(msg, suffix);
+
+    // Comandos Kitty-cat
+    case 'banho':
+      return banho(msg, suffix);
+    case 'sopa':
+      return sopa(msg, suffix);
+    case 'beijo':
+      return beijo(msg, suffix);
+
+    // Comandos Suporte Adm
+    case 'ajuda':
+      return ajuda(msg, suffix);
+    case 'kick':
+      return kick(msg, suffix);
+    case 'delet':
+      return delet(msg, suffix);
+    default:
+      msg.channel.send(basicembed('5351170', 'Comando errado ' + msg.author + ' b-baka...'));
     }
   }
 
-  // Template Mensagem Básica
-  function basicembed(color, text) {
+    if ((/~lolicon/).test(message.content)) {
+      const mention = message.mentions.users.first();
+      message.channel.send({embed: {
+        color: 5351170,
+        description: mention.toString() + ", O-onii-san"
+      }});
+      message.delete().then(msg => console.log(`Deleted message from ${msg.author}`)).catch(console.error);
+    }
+  });
+
+  function basicembed(color,text) {
     return {embed: {
       color: color,
       description: text
       }};
   }
 
-  // Template Mensagem com Imagem
-  function imageembed(color, image, text) {
+  function imageembed(color,image,text) {
   return {embed: {
     "color": color,
     "description": text,
@@ -57,7 +88,7 @@
   function ohayo(msg, suffix) {
     msg.delete();
     const mention = msg.mentions.users.first();
-    msg.channel.send(basicembed('cor-destaque', 'Ohayo ' + mention.toString()));
+    msg.channel.send(basicembed('5351170', 'Ohayo ' + mention.toString()));
   }
 
   function isAdmin(member) {
@@ -67,10 +98,29 @@
   function permission(msg, suffix) {
     msg.delete();
     if (isAdmin(msg.member)) {
-      msg.channel.send(basicembed('cor-destaque', 'Com permissão, nha'));
+      msg.channel.send(basicembed('5351170', 'Com permissão, nha'));
     } else {
-      msg.channel.send(basicembed('cor-destaque', 'Sem permissão'));
+      msg.channel.send(basicembed('5351170', 'Sem permissão'));
     }
+  }
+
+  function banho(msg, suffix) {
+    msg.delete();
+    const mention = msg.mentions.users.first();
+    msg.channel.send(imageembed('5351170', 'https://68.media.tumblr.com/cbad86fd71e480486c1d6ef983a03b97/tumblr_o4jxlkyrMd1rc0rvzo1_500.png', mention.toString() + ' está dando banho na ' + Bot.user));
+  }
+
+  function beijo(msg, suffix) {
+    msg.delete();
+    const mention = msg.mentions.users.first();
+    msg.channel.send(imageembed('5351170', 'https://i.ytimg.com/vi/_IRuDIsj3vE/maxresdefault.jpg', mention.toString() + ' deu beijo indireto na ' + Bot.user));
+  }
+
+  function kiss(msg, suffix) {
+    msg.delete();
+    const mention = msg.mentions.users.first();
+    const mention_other = msg.mentions.users.last();
+    msg.channel.send(basicembed('5351170', mention.toString() + ' deu um beijo em ' + mention_other.toString()));
   }
 
   function ship(msg, suffix) {
@@ -86,38 +136,51 @@
     const ship_trimTwo = strmention_other.substring(4);
     const ship_name = ship_trimOne + ship_trimTwo;
 
-    msg.channel.send(basicembed('cor-destaque', ship_name));
+    msg.channel.send(basicembed('5351170', ship_name));
   }
 
-  function delet(msg, suffix) {
-    const messages = msg.channel.DownloadMessages(5);
-    msg.DeleteMessages(messages);
+  function say(msg, suffix) {
+    msg.delete();
+    msg.channel.send(basicembed('5351170', suffix));
+  }
+
+  function onii(msg, suffix) {
+    msg.delete();
+    msg.channel.send(basicembed('5351170', mention.toString() + ', O-onii-san'));
   }
 
   function ajuda(msg, suffix) {
     msg.delete();
     msg.channel.send({embed: {
-      color: cor-destaque,
+      color: 3447003,
       author: {
         name: Bot.user.username,
         icon_url: Bot.user.avatarURL
       },
-      title: "COMANDOS DA IKA-CHAN",
+      title: "COMANDOS DA KITTY-CAT",
       description: "B-baka :heart: ",
       fields: [{
-        name: "Em Desenvolvimento",
-        value: "Em Desenvolvimento"
+        name: "!banho @user",
+        value: "@user da banho na Kitty"
+      },
+      {
+        name: "RABBITÃO",
+        value: "[Rabbit - iFonteSeca](https://www.rabb.it/ifonteseca) \r[Rabbit - MaVi](https://www.rabb.it/MaVi) "
       },
       ],
       timestamp: new Date(),
       footer: {
         icon_url: Bot.user.avatarURL,
-        text: "© Ika-chan"
+        text: "© Kitty-Cat"
       }
     }
   });
   }
 
+    function delet(msg, suffix) {
+      const messages = msg.channel.DownloadMessages(5);
 
+      msg.DeleteMessages(messages);
+    }
 
   Bot.login(token);
